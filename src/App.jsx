0e2a38886263e9d0755fd0a43ad6c1f996dc5e0a
mdtspramanik <189224128+mdtspramanik@users.noreply.github.com>
2025-05-01 00:13:08 +0600

    feat(header): add header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import { useState } from "react";
+import Search from "./components/search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero Banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without the Hassle
+            </h1>
+          </header>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </div>
+      </div>
+    </main>
   );
 };
 
